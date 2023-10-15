6b02ab3b1b678e152b61e004899ef758681aa1bd
Zain Shakoor <92949137+zainsh098@users.noreply.github.com>
2023-10-15 14:55:00 +0500

    working on main file for printing ,input etc

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,32 @@
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter Stack Size ");
+        Scanner inputStackNumber=new Scanner(System.in);
+        int stackSize= input.nextInt();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        System.out.println("Enter Stack Numbers :");
+        StackArray stackArray=new StackArray(stackSize);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+
+
+
+        for (int i=0;i<=stackSize;i++)
+        {
+            stackArray.pushNumber(inputStackNumber.nextInt());
+
+
         }
+
+
+        stackArray.printStack();
+
+
+
+
     }
 }
