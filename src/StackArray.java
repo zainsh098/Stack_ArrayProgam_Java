60b20ea8f5759a4c527703f6820f0d3a0825e016
Zain Shakoor <92949137+zainsh098@users.noreply.github.com>
2023-10-15 14:39:40 +0500

    Code a logic for diff operations of Stack

--- src/StackArray.java
+++ src/StackArray.java
@@ -1,2 +1,61 @@
-package PACKAGE_NAME;public class StackArray {
+
+public class StackArray {
+
+    private  int[] stack;
+    private  int top;
+
+    public  StackArray(int size)
+    {
+        stack=new int[size];
+        top=-1;
+
+
+    }
+    public  void pushNumber(int num)
+    {
+        if(isFull())
+        {
+            System.out.println("Stack is Full");
+            return;
+
+        }
+        stack[++top]=num;
+
+
+    }
+    public  int pop()
+    {
+        if(isEmpty())
+        {
+            System.out.println("Stack is Empty");
+                    return -1;
+        }
+        return  stack[top--];
+
+    }
+    public  boolean isEmpty()
+    {
+        return  top==-1;
+
+    }
+    public boolean isFull()
+    {
+        return  top==stack.length-1;
+
+    }
+
+    public  void printStack()
+    {
+        for (int i=top;i>0;i--)
+        {
+            System.out.println(stack[i]);
+
+
+        }
+
+
+    }
+
+
+
 }
